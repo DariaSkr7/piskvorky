73526ae27b82dc27517631e5461c7f0c312baab7
Daria Skoryk <dariaskoryk@gmail.com>
2023-10-13 19:51:49 +0200

    final result-part 4

--- js/game.js
+++ js/game.js
@@ -1,21 +1,45 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
 const playerElement = document.querySelector('.player');
+
+const gameBoard = Array.from({ length: 100 }, (_, i) => {
+  return '_';
+});
+
+const whoIsWinner = () => {
+  const winner = findWinner(gameBoard);
+  if (winner === 'o' || winner === 'x') {
+    alert(`Vyhrál hráč se symbolem ${winner}.`);
+    location.reload();
+  }
+  if (winner === 'tie') {
+    alert(`Hra scončila neroyhodně.`);
+    location.reload();
+  }
+};
 
 const play = (event) => {
   event.target.classList.add(`board__field--${currentPlayer}`);
   event.target.disabled = true;
 
+  const index = Number(event.target.id);
+
   if (currentPlayer === 'circle') {
     currentPlayer = 'cross';
+    gameBoard[index] = 'o';
   } else {
     currentPlayer = 'circle';
+    gameBoard[index] = 'x';
   }
 
   playerElement.src = `./img/${currentPlayer}-white.svg`;
+  setTimeout(whoIsWinner, 200);
 };
 
 const cells = document.querySelectorAll('.cell');
-cells.forEach((cell) => {
+cells.forEach((cell, index) => {
+  cell.id = index;
   cell.addEventListener('click', play);
 });
 
